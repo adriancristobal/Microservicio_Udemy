4af64f1741774d91c33338e93957ea0945a21c12
adriancristobal <cristobala1309@gmail.com>
2023-12-17 17:41:04 +0100

    Configuration IDE with SpringBoot

---
+++ README.md
@@ -0,0 +1,2 @@
+# Microservicio_Udemy
+ Microservices project in which I will use technologies such as SpringBoot, Docker, SpringCloud... among others
---
+++ src/main/java/com/udemy/MicroservicioUdemyApplication.java
@@ -0,0 +1,12 @@
+package com.udemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioUdemyApplication.class, args);
+	}
+}
