85f9f43c1033f441713ad09cd0b4596c1b03ae02
adriancristobal <cristobala1309@gmail.com>
2023-12-17 22:54:23 +0100

    Config Spring
    Config Spring Properties
    Add endpoints for XML and JSON

--- src/main/java/com/udemy/MicroservicioUdemyApplication.java
+++ src/main/java/com/udemy/MicroservicioUdemyApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// In case you have a class outsite the package of SpringApplication, you have to indicate here, if not, it will not take that class
+// @ComponentScan(basePackages = {packageName})
 public class MicroservicioUdemyApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/udemy/TestController.java
+++ src/main/java/com/udemy/TestController.java
@@ -1,14 +1,39 @@
 package com.udemy;
 
 import org.springframework.http.MediaType;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+
+/**
+ * <h1> Example of controllers </h1>
+ * 
+ * @author cristobalr1309@gmail.com
+ * @version 1.0
+ * @since 17/12/2023
+ */
 @RestController
 public class TestController {
 
-	@GetMapping(value = "saludo", produces = MediaType.TEXT_PLAIN_VALUE)
-	public String testController() {
-		return "Hola test";
+	//<<-- Return text -->>
+	@GetMapping(value = "greeting/text", produces = MediaType.TEXT_PLAIN_VALUE)
+	public String testTextController() {
+		return "Hello World!";
+	}
+	
+	//<<-- Return XML -->>
+	// If you use a class model in the @RequestParam, you have to anotate that class with @JacksonXmlRootElement or @XmlRootElement( (i dont remember)
+	@GetMapping(value = "greeting/xml/{name}", produces = MediaType.APPLICATION_XML_VALUE)
+	public String testXMLController(@PathVariable("name") String username) {
+		return "Hello " + username;
+	}
+	
+	//<<-- Return JSON -->>
+	@GetMapping(value = "greeting/json/{name}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public String testJSONController(@PathVariable String name) {
+		return "Hello " + name;
 	}
 }
