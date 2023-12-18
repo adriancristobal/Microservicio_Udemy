afb8b751dcc5034fc30f84edff3a781be60c7681
adriancristobal <cristobala1309@gmail.com>
2023-12-18 23:50:52 +0100

    Last changes
    Mock config

--- src/main/java/com/udemy/TestController.java
+++ src/main/java/com/udemy/TestController.java
@@ -6,32 +6,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
-
 /**
  * <h1> Example of controllers </h1>
  * 
  * @author cristobalr1309@gmail.com
  * @version 1.0
- * @since 17/12/2023
+ * @since 2023-12-17
  */
 @RestController
 public class TestController {
 
-	//<<-- Return text -->>
+	// <<--Return text-->>
 	@GetMapping(value = "greeting/text", produces = MediaType.TEXT_PLAIN_VALUE)
 	public String testTextController() {
 		return "Hello World!";
 	}
-	
-	//<<-- Return XML -->>
-	// If you use a class model in the @RequestParam, you have to anotate that class with @JacksonXmlRootElement or @XmlRootElement( (i dont remember)
+
+	// <<--Return XML-->>
 	@GetMapping(value = "greeting/xml/{name}", produces = MediaType.APPLICATION_XML_VALUE)
 	public String testXMLController(@PathVariable("name") String username) {
 		return "Hello " + username;
 	}
-	
-	//<<-- Return JSON -->>
+
+	// <<-- Return JSON -->>
 	@GetMapping(value = "greeting/json/{name}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public String testJSONController(@PathVariable String name) {
 		return "Hello " + name;
--- src/test/java/com/udemy/MicroservicioUdemyApplicationTests.java
+++ src/test/java/com/udemy/MicroservicioUdemyApplicationTests.java
@@ -1,13 +1,47 @@
 package com.udemy;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc //is of spring
+@TestMethodOrder(OrderAnnotation.class) //the tests are shown by order
 class MicroservicioUdemyApplicationTests {
 
+	@Autowired
+	MockMvc mock;
+	
 	@Test
-	void contextLoads() {
+	@Order(1)
+	void getGreetingJSONTest() throws Exception {
+		mock.perform(get("/greeting/json/jdoe")).andDo(print());
 	}
-
+	
+	@Test
+	@Order(0)
+	void getGreetingXMLTest() throws Exception {
+		mock.perform(get("/greeting/xml/Jhon")).andDo(print());
+	}
+	
+	
+//	@Test
+//	@Order(3)
+//	void postExampleTest() throws Exception {
+//		mock.perform(post("/greeting/xml/Jhon")
+//				.contentType(MediaType.APPLICATION_JSON)
+//				.content("{\"nombre\":\"exampleName\"}")
+//				).andDo(print());
+//	}
+	
+	
+	
 }
